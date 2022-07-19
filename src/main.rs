b72bdececdf0aa8811c76afae7cda4d4be13c342
Kurtsley <kurtsley@gmail.com>
2022-07-18 22:53:00 -0500

    comments

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use tui::{
 use tui_image::Image;
 
 fn ui<B: Backend>(f: &mut Frame<B>) {
+    // Save image
     const IMAGE: &[u8] = include_bytes!("../res/xno-light.png");
 
     let size = f.size();
@@ -30,7 +31,7 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
         .expect("Couldn't load image")
         .into_rgba8();
 
-    // Surrounding block
+    // Main block
     let block = Block::default()
         .borders(Borders::ALL)
         .title(Span::styled(
@@ -41,18 +42,21 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
         .border_type(BorderType::Rounded);
     f.render_widget(block, size);
 
+    // Vertical chunk split
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .margin(2)
         .constraints([Constraint::Percentage(90), Constraint::Percentage(10)].as_ref())
         .split(f.size());
 
+    // Top chunks
     let top_chunks = Layout::default()
         .direction(Direction::Horizontal)
         .margin(2)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
         .split(chunks[0]);
 
+    // Info chunk vertical split
     let chunk_left = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
@@ -62,8 +66,8 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
         ])
         .split(top_chunks[1]);
 
+    // Info blocks
     let block_info_middle = Block::default();
-
     let block_blank_top = Block::default();
     let block_blank_bottom = Block::default();
 
@@ -74,6 +78,7 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
         .title_alignment(Alignment::Center);
     f.render_widget(block_info_main, top_chunks[1]);
 
+    // Info text
     let text = vec![
         Spans::from(""),
         Spans::from(Span::styled(
@@ -123,6 +128,7 @@ fn ui<B: Backend>(f: &mut Frame<B>) {
     f.render_widget(text_info, chunk_left[1]);
     f.render_widget(block_blank_bottom, chunk_left[2]);
 
+    // Logo
     let block_logo = Block::default()
         .borders(Borders::ALL)
         .border_type(BorderType::Rounded);
@@ -152,6 +158,7 @@ struct Data {
     percent_change_24h: f32,
 }
 
+// Query kurtsley.net and return data or error.
 fn get_data() -> Result<Data, reqwest::Error> {
     let url = "https://www.kurtsley.net";
     let res = reqwest::blocking::get(url)?.json::<Data>()?;
